46aed12f6e1789f84d11f720edf252f0dfaf2556
Tim Iland <tim.iland@deptagency.com>
2023-08-17 15:38:20 +0100

    committing to look better

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,15 @@ import BookCard from "./Components/BookCard";
 function App() {
   return (
     <div className="App">
-      <BookCard highlighted title="Title" votes={10} image="" />
+      <div className="w-full p-8 text-green-500 bg-black text-2xl font-medium">
+        THE TIMBO JIMBO BOOK POLL
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 container mx-auto gap-8 pt-12">
+        <BookCard highlighted title="Title" votes={10} image="" />
+        <BookCard highlighted title="Title" votes={10} image="" />
+        <BookCard highlighted title="Title" votes={10} image="" />
+        <BookCard highlighted title="Title" votes={10} image="" />
+      </div>
     </div>
   );
 }
--- src/Components/BookCard.tsx
+++ src/Components/BookCard.tsx
@@ -6,8 +6,8 @@ interface IBookCard extends IBook {
 
 const BookCard = ({ highlighted, title, votes, image }: IBookCard) => {
   return (
-    <div>
-      <h1>Book Title</h1>
+    <div className="p-8 rounded-xl border border-green-500 bg-black text-white">
+      <h1 className="text-green-500">Book Title</h1>
       <img
         alt="book-title"
         src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1673458354i/62296528.jpg"
